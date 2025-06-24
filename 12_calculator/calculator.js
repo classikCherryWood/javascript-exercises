3630d3bedf1acce28bd355eb699075b46c0364e6
classikCherryWood <chan20010725@gmail.com>
2025-06-24 14:10:21 +0100

    ex12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(...args) {
+  let sum = args.reduce((prev, next) => prev + next, 0);
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  if (!args){
+    return 0;
+  } else {
+    return args.reduce((prev, next) => prev + next, 0);
+  }
 };
 
-const multiply = function() {
+const multiply = function(args) {
+  return args.reduce((prev, next) => prev * next, 1);   
 
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  return (base**power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  else {
+    let prod = 1;
+    for (let i = num; i > 0; i--){
+      prod *= i;
+    }
+    return prod;
+  }
 };
 
 // Do not edit below this line
