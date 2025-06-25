840392e281b0eb622e9fcc8b878781bb66bca7f5
classikCherryWood <chan20010725@gmail.com>
2025-06-25 12:25:09 +0100

    ex 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    // let ageArr = people.map((person) => ({
+    //     name: `${person['name']}`, 
+    //     age: `${person['yearOfDeath']}` - `${person['yearOfBirth']}`
+    // }));
+
+    let ageArr = people.map(function(person) {
+        if (person.yearOfDeath){
+            return ({
+                name: `${person['name']}`, 
+                age: `${person['yearOfDeath']}` - `${person['yearOfBirth']}`
+            })
+        } else {
+            return ({
+                name: `${person['name']}`, 
+                age: `${new Date().getFullYear()}` - `${person['yearOfBirth']}`
+                
+            })
+        }
+    })
+
+
+    let sorted = ageArr.sort((a, b) => a.age > b.age ? -1:1);
+    return sorted[0];
+
 
 };
 
