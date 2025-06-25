03a7cfc953d046ef4168e27691259bbd32c956e7
classikCherryWood <chan20010725@gmail.com>
2025-06-25 11:21:16 +0100

    ex13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+    // condition filter: alphanumerical
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz1234567890";
+
+    // split into array
+    const cleaned = str
+        .toLowerCase()
+        .split('')
+        .filter((element) => alphanumerical.includes(element))
+        .join(''); 
+
+    const reverseCleaned = cleaned.split('').reverse().join('');
+
+    return cleaned === reverseCleaned;
+
 
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
